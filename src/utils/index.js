45508c87f45a34bcceb60aa4319ecc24fed976c8
Yuki Shibata <yukiyohure@gmail.com>
2021-03-24 21:31:43 +0900

    format the string type to the date type using format() and parse() of date-fns

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
-import { format } from "date-fns";
+import { parse, format } from "date-fns";
 
 export const getFormatedDate = (dateString) => {
   // dateString: 2021-03-12T14:40:54Z
-  // format()の構文 : format(date, format, [options]);
-  // https://date-fns.org/v2.19.0/docs/format#arguments
-  return format(new Date(dateString), "yyyy-MM-dd");
+  // parse()の構文: parse(dateString, formatString, referenceDate, [options])
+  // format()の構文: format(date, format, [options]);
+  const parsedDate = parse(dateString, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
+  return format(parsedDate, "yyyy-MM-dd");
 };
 
 export const validateRequired = (value, errorMessage) => {
